331651cc7e48eeb32c0a82451f69204c73985b59
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-12-10 11:54:40 -0800

    Just some basic packages and the start of some classes

---
+++ NetworkTable.py
@@ -0,0 +1,11 @@
+from networktables import NetworkTables as nt
+import time
+
+# connect to the pynetworktable server
+nt.initialize(server="robotio-XX-frc.local")
+
+# --------------------------------------------------------------------------------
+# todo
+# Write a class for getting and setting data in networktable
+# https://robotpy.readthedocs.io/projects/pynetworktables/en/stable/api.html
+# --------------------------------------------------------------------------------
